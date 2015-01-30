884f323a80ec0fe79e6193fed5abd0e4d8db22d7
Colin Woodbury <colingw@gmail.com>
2015-01-30 01:24:51 -0800

    (404 - A1) Basically works but needs improvement
    - Still needs a lot of hints before it can produce decent results.

--- cmpt404/decrypt.hs
+++ cmpt404/decrypt.hs
@@ -1,11 +1,4 @@
 {-
-I want to choose some mapping, and calculate the sum of its positive
-statistical distances.
-In theory, the mapping with the smallest total distance will be
-the correct one.
-
-Need to allow for assumptions.
-
 ALG:
 - Remove assumptions from possible map targets.
 - For each cipher letter, sort the remaining targets
@@ -22,12 +15,10 @@ ALG:
 
 module Decrypt where
 
-import           Control.Applicative ((<$>))
 import           Control.Lens
 import           Control.Monad
 import           Data.List (sort,sortBy)
 import qualified Data.Map.Lazy as M
-import           Data.Maybe (fromJust)
 import           Data.Monoid
 import           Data.Text (Text)
 import qualified Data.Text as T
@@ -37,38 +28,109 @@ import           System.Random.Shuffle
 
 ---
 
-data Letter = L { letter    :: Char
-                , target    :: Maybe Char
-                , potential :: [(Char,Float)] } deriving (Eq,Show)
-
-targetL :: Lens' Letter (Maybe Char)
-targetL f (L l t p) = fmap (\t' -> L l t' p) $ f t
-
-potentialL :: Lens' Letter [(Char,Float)]
-potentialL f (L l t p) = fmap (\p' -> L l t p') $ f p
-
+type Distance  = Float
 type Frequency = (Char,Float)
 
-data Mapping = M Char Float Char Float deriving (Eq,Show)
+data Pair = P { pLetter :: Char
+              , target  :: (Char,Distance) } deriving (Eq,Show)
+
+targetL :: Lens' Pair (Char,Distance)
+targetL f (P l t) = fmap (\t' -> P l t') $ f t
+
+pairL :: Lens' Pair (Char,Char)
+pairL f (P p (t,d)) = fmap (\(p',t') -> P p' (t',d)) $ f (p,t)
+
+data Letter = L { lLetter   :: Char
+                , potential :: [(Char,Distance)] } deriving (Eq,Show)
+
+potentialL :: Lens' Letter [(Char,Distance)]
+potentialL f (L l p) = fmap (\p' -> L l p') $ f p
+
+-- | A list of Char-to-Char mappings, with the total Statistical Distance.
+data Mapping = M { pairs    :: [Pair]
+                 , distance :: Float } deriving (Eq,Show)
+
+pairsL :: Lens' Mapping [Pair]
+pairsL f (M p d) = fmap (\p' -> M p' d) $ f p
 
 ---
 
----
+j = do
+  c  <- TIO.readFile "longCipher.txt"
+  g  <- newStdGen
+  let cs = textFreqs c
+      (rs',cs') = prune assumptions realFreqs cs
+      ls = letters rs' cs'
+      ms = take 100 . sortBy p . allMappings g 5000 ls $ assumptions
+  print cs
+  print $ map distance ms
+  print $ (head ms) ^.. pairsL . traverse . pairL
+  return $ decrypt c $ head ms
+  where p (M _ d1) (M _ d2) = compare d1 d2
 
--- | Given assumptions, prunes those characters from the remaining
--- cipher letters, as well as the potential targets.
-prune :: [Letter] -> [Frequency] -> [Frequency] -> ([Frequency],[Frequency])
-prune ls rs cs = (rs',cs')
-  where rs' = rs ^.. traverse . filtered (flip notElem ts . fst)
-        cs' = cs ^.. traverse . filtered (flip notElem ls' . fst)
-        ls' = map letter ls
-        ts  = ls ^.. traverse . targetL . _Just
+decrypt :: Text -> Mapping -> Maybe Text
+decrypt t m = fmap T.pack . mapM f . T.unpack . T.unwords . T.lines $ t
+  where f ' ' = Just ' '
+        f c   = c `M.lookup` m'
+        m'    = M.fromList $ m ^.. pairsL . traverse . pairL
+
+-- Most common: e t a o i n s h r d l
+assumptions :: [Pair]
+assumptions = [ P 'h' ('e',0)
+              , P 'w' ('t',0)
+              , P 'd' ('a',0)
+              , P 'l' ('i',0)
+              , P 'q' ('n',0)
+              , P 'k' ('h',0)
+              , P 'o' ('l',0) ]
+
+{-}
+assumptions = [ P 'h' ('e',0)
+              , P 'd' ('a',0)
+              , P 'k' ('h',0)                
+              , P 'l' ('i',0)
+              , P 's' ('n',0)
+              , P 't' ('o',0)
+              , P 'w' ('r',0)
+              , P 'x' ('s',0)
+              , P 'y' ('t',0) ]
+              , P 'a' ('k',0)
+              , P 'f' ('c',0)
+              , P 'b' ('l',0)
+              , P 'u' ('p',0)
+              , P 'i' ('f',0)
+              , P 'z' ('u',0)
+              , P 'c' ('m',0) ] -}
+
+allMappings :: StdGen -> Int -> [Letter] -> [Pair] -> [Mapping]
+allMappings g n ls as = f g [1..n]
+  where lsLen = length ls
+        f _ [] = []
+        f g' (_:ns) = 
+          let ls' = shuffle' ls lsLen g'
+              ps  = fst $ foldl allMappings' ([],[]) ls'
+              d   = sum $ ps ^.. traverse . targetL . _2
+          in M (as ++ ps) d : f (snd $ next g') ns
+
+allMappings' :: ([Pair],[Char]) -> Letter -> ([Pair],[Char])
+allMappings' (acc,u) l = (P (lLetter l) c : acc, fst c : u)
+  where l' = l & potentialL %~ filter (flip notElem u . fst)
+        c  = l' ^. potentialL . to head
 
 -- `cs` and `rs` need the same length.
 letters :: [Frequency] -> [Frequency] -> [Letter]
 letters rs cs = map f cs
-  where f (c,f1) = L c Nothing . sortBy p . map (_2 %~ (abs . (f1 -))) $ rs
+  where f (c,f1) = L c . sortBy p . map (_2 %~ (abs . (f1 -))) $ rs
         p (_,f1) (_,f2) = compare f1 f2
+
+-- | Given assumptions, prunes those characters from the remaining
+-- cipher letters, as well as the potential targets.
+prune :: [Pair] -> [Frequency] -> [Frequency] -> ([Frequency],[Frequency])
+prune ps rs cs = (rs',cs')
+  where rs' = rs ^.. traverse . filtered (flip notElem ts . fst)
+        cs' = cs ^.. traverse . filtered (flip notElem ls' . fst)
+        ls' = map pLetter ps
+        ts  = ps ^.. traverse . targetL . _1
 
 -- | At what frequency does each character appear in the cipher text?
 textFreqs :: Text -> [Frequency]
@@ -86,6 +148,7 @@ textFreqs t = map f items
 items :: [Char]
 items = ['a' .. 'z']
 
+-- e t a o i n s h r d l
 -- Borrowed from https://en.wikipedia.org/wiki/Letter_frequency
 realFreqs :: [Frequency]
 realFreqs = [ ('a',8.167)
@@ -118,45 +181,3 @@ realFreqs = [ ('a',8.167)
 -- | Sort a `Text` value.
 sortT :: Text -> Text
 sortT = T.pack . sort . T.unpack
-
--- | Sort by letter frequency. *DEAD*
---sortF :: [Frequency] -> [Frequency]
---sortF = sortBy (\(_,f1) (_,f2) -> compare f2 f1)
-
----
-
-{-}
-decrypt :: Text -> Maybe Text
-decrypt t = fmap T.pack . mapM m . T.unpack . T.unwords . T.lines $ t
-  where m = mapping (textFreqs t) realFreqs
-
-distance :: [Mapping] -> Float
-distance = sum . map distance'
-  where distance' (M _ f1 _ f2) = if f1 - f2 > 0 then f1 - f2 else 0
-
--- | Maps Chars from one distribution to another.
-mapping :: [Frequency] -> [Frequency] -> Char -> Maybe Char
-mapping _ _ ' ' = Just ' '
-mapping f1 f2 c = M.lookup c $ pairing f1 f2
-
-pairing :: [Frequency] -> [Frequency] -> M.Map Char Char
-pairing f1 = M.fromList . zipWith (\(c1,_) (c2,_) -> (c1,c2)) f1
--}
-
---f = decrypt <$> c
-g = textFreqs <$> c
---h = (`pairing` realFreqs) <$> g
-i = shuffle' [1..10] 10 <$> newStdGen
-c = TIO.readFile "cipher.txt"
-
-j = do
-  cs <- textFreqs <$> c
-  let (rs',cs') = prune assumptions realFreqs cs
-      ls = letters rs' cs'
-  return ls
-
-assumptions :: [Letter]
-assumptions = [ L 'h' (Just 'e') []
-              , L 'd' (Just 'a') [] ]
-
--- use `next` to get another generator from one you have
