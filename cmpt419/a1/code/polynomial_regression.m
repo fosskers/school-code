590b7fca8181e3bb1b57d97fd6903d2257eee938
Colin Woodbury <colingw@gmail.com>
2015-10-07 14:20:31 -0700

    (419 - A1) Initial regression correct (?)

--- cmpt419/a1/code/polynomial_regression.m
+++ cmpt419/a1/code/polynomial_regression.m
@@ -3,32 +3,44 @@
 % Solution to Question 4.2.1
 % Author: Colin Woodbury <cwoodbur@sfu.ca>
 
+% Make sure workspace is empty.
+clear;
+
 % Load the data
 [Countries, Features, Data] = loadUnicefData();
 
 % Split into training and testing data
 t = Data(:,2);
+tTr = t(1:100);
+tTe = t(101:end);
 X = Data(:,8:end);
-%X = normalizeData(X);
+X = normalizeData(X);
+XTr = X(1:100,:);
+XTe = X(101:end,:);
 
-% Try it with deg 1 for now.
-Phi = designMatrix(X,'polynomial',1);
-w = pinv(Phi) * t;
+errM = [];
+DEG_MAX = 6;
 
-% Squared Error
-err = 0;
-for i=1:size(X,1)
-    % Apply phi to x's
-    phid = [1];  % phi0 = const 1
-    for j=1:size(X(i,:),2)
-        phid(j+1,1) = X(i,j) ^ 1; % degree 1 for now
-    end
+for deg=1:DEG_MAX
+    PhiTr = designMatrix(XTr,'polynomial',deg);
+    w = pinv(PhiTr) * tTr;
 
-    % Accumulate error
-    err = err + (t(i) - w' * phid) ^ 2;
+    % Squared Error for Training Data
+    err = sum((tTr - PhiTr * w) .^ 2);
+
+    % RMS training error
+    errM(1,deg) = sqrt(err / size(XTr,1));
+
+    % Squared Error for Test Data
+    err = 0;
+    PhiTe = designMatrix(XTe,'polynomial',deg);
+    err = sum((tTe - PhiTe * w) .^ 2);
+
+    % RMS testing Error
+    errM(2,deg) = sqrt(err / size(XTe,1));
 end
 
-% Final error value
-err = err * 0.5;
-
-plot(1:6,[err 0 0 0 0 0]);
+plot(1:DEG_MAX,errM);
+ylabel('Error');
+xlabel('Polynomial Degree');
+legend('Training','Testing','Location','northwest');
