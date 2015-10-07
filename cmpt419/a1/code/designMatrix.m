f8f4045caaa7b1b1877961f2ce1f5e93e0b75bda
Colin Woodbury <colingw@gmail.com>
2015-10-07 10:07:55 -0700

    (419 - A1) Plot for degree 1 appears

--- cmpt419/a1/code/designMatrix.m
+++ cmpt419/a1/code/designMatrix.m
@@ -8,9 +8,17 @@ function Phi = designMatrix(X,basis,varargin)
 % Mu is k-by-1
 
 if strcmp(basis,'polynomial')
-  k = varargin{1};
-  % TO DO:: Fill in
-  Phi = []
+  deg = varargin{1};
+
+  % Initialize as a column vector of 1s. These are the x_0 values
+  % given to `phi0 = const 1`.
+  Phi(1:size(X,1),1) = 1;
+  
+  for i=1:size(X,1)
+      for j=1:size(X,2)
+          Phi(i,j+1) = X(i,j) ^ deg;
+      end
+  end
 elseif strcmp(basis,'sigmoid')
   Mu = varargin{1};
   s = varargin{2};
--- cmpt419/a1/code/polynomial_regression.m
+++ cmpt419/a1/code/polynomial_regression.m
@@ -1,6 +1,7 @@
 % polynomial_regression.m
 %
 % Solution to Question 4.2.1
+% Author: Colin Woodbury <cwoodbur@sfu.ca>
 
 % Load the data
 [Countries, Features, Data] = loadUnicefData();
@@ -10,4 +11,24 @@ t = Data(:,2);
 X = Data(:,8:end);
 %X = normalizeData(X);
 
-% TO DO:: Fill in
+% Try it with deg 1 for now.
+Phi = designMatrix(X,'polynomial',1);
+w = pinv(Phi) * t;
+
+% Squared Error
+err = 0;
+for i=1:size(X,1)
+    % Apply phi to x's
+    phid = [1];  % phi0 = const 1
+    for j=1:size(X(i,:),2)
+        phid(j+1,1) = X(i,j) ^ 1; % degree 1 for now
+    end
+
+    % Accumulate error
+    err = err + (t(i) - w' * phid) ^ 2;
+end
+
+% Final error value
+err = err * 0.5;
+
+plot(1:6,[err 0 0 0 0 0]);
