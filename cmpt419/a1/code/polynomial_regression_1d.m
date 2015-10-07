8cc1204174418a9ae6b5a8fe2877ac7dcf7cc77f
Colin Woodbury <colingw@gmail.com>
2015-10-07 16:21:13 -0700

    (419 - A1) Visualize some data

--- cmpt419/a1/code/polynomial_regression_1d.m
+++ cmpt419/a1/code/polynomial_regression_1d.m
@@ -1,9 +1,46 @@
 % polynomial_regression_1d.m
 %
 % Solution to Question 4.2.2
+% Author: Colin Woodbury <cwoodbur@sfu.ca>
+
+% Make sure workspace is empty.
+clear;
 
 % Load the data
 [Countries, Features, Data] = loadUnicefData();
 
+% Split into training and testing data
+t = Data(:,2);
+tTr = t(1:100);
+tTe = t(101:end);
+X = Data(:,8:15);
+X = normalizeData(X);
+XTr = X(1:100,:);
+XTe = X(101:end,:);
 
-% TO DO:: Fill in
+errM = [];
+COLS = 8;
+
+for col=1:COLS
+    PhiTr = designMatrix(XTr(:,col),'polynomial',3);
+    w = pinv(PhiTr) * tTr;
+
+    % Squared Error for Training Data
+    err = sum((tTr - PhiTr * w) .^ 2);
+
+    % RMS training error
+    errM(1,col) = sqrt(err / size(XTr,1));
+
+    % Squared Error for Test Data
+    err = 0;
+    PhiTe = designMatrix(XTe(:,col),'polynomial',3);
+    err = sum((tTe - PhiTe * w) .^ 2);
+
+    % RMS testing Error
+    errM(2,col) = sqrt(err / size(XTe,1));
+end
+
+bar(8:15,errM');
+ylabel('Error');
+xlabel('Features');
+legend('Training','Testing','Location','northwest');
--- cmpt419/a1/code/visualize_1d.m
+++ cmpt419/a1/code/visualize_1d.m
@@ -1,33 +1,40 @@
 % X is 1-d
-% X_train, X_test, t_train, t_test should all be 1-d, and need to be defined as well.
-% You should modify y_ev
+% X_train, X_test, t_train, t_test should all be 1-d, and need to
+% be  defined as well. You should modify y_ev.
 
 [Countries, Features, Data] = loadUnicefData();
 
 % Split into training and testing data
 t = Data(:,2);
-X = Data(:,12);
 
-ntrain = 100;
-X_train = X(1:ntrain);
-X_test = X(ntrain+1:end);
-t_train = t(1:ntrain);
-t_test = t(ntrain+1:end);
+DEG = 3;
 
+for i=11:13
+    X = Data(:,i);
 
-% Plot a curve showing learned function.
-x_ev = (min(X):0.1:max(X))';
+    ntrain = 100;
+    X_train = X(1:ntrain);
+    X_test = X(ntrain+1:end);
+    t_train = t(1:ntrain);
+    t_test = t(ntrain+1:end);
 
-% TO DO:: Put your regression estimate here.
-y_ev = 100*sin(x_ev);
+    % Plot a curve showing learned function.
+    x_ev = (min(X):0.1:max(X))';
 
-figure;
-plot(x_ev,y_ev,'r.-');  
-hold on;
-plot(X_train,t_train,'g.');
-plot(X_test,t_test,'bo');
-hold off;
-title(sprintf('Fit with degree %d polynomial',5));
-% Make the fonts larger, good for reports.
-set(findall(gcf,'type','text'),'FontSize',20)
-set(findall(gcf,'type','axes'),'FontSize',20)
+    Phi = designMatrix(X_train,'polynomial',DEG);
+    w = pinv(Phi) * t_train;
+
+    y_ev = designMatrix(x_ev,'polynomial',DEG) * w;
+
+    figure;
+    plot(x_ev,y_ev,'r.-');  
+    hold on;
+    plot(X_train,t_train,'g.');
+    plot(X_test,t_test,'bo');
+    hold off;
+    title(sprintf('Feature %d Data and Fitted Curve',i));
+    % Make the fonts larger, good for reports.
+    set(findall(gcf,'type','text'),'FontSize',20)
+    set(findall(gcf,'type','axes'),'FontSize',20)
+end
+
