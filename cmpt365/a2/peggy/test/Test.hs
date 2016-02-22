5e14ef048f1ad3065f1cdff270ddf001b35a3e1a
Colin Woodbury <colingw@gmail.com>
2016-02-21 16:50:32 -0800

    (365 - A2) Streamlined isomorphism testing

--- cmpt365/a2/peggy/test/Test.hs
+++ cmpt365/a2/peggy/test/Test.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Data.Matrix as M
 import Data.Vector as V
+import Data.Word (Word8)
 import JPEG
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -11,16 +12,17 @@ import Test.Tasty.HUnit
 suite :: TestTree
 suite = testGroup "Unit Tests"
   [ testGroup "Colour Conversions"
-    [ testCase "RGB -> YCbCr Isomorphism" iso
+    [ testCase "RGB -> YCbCr Isomorphism" $ iso (0,0,0)
+    , testCase "RGB -> YCbCr Isomorphism" $ iso (110,115,200)
+    , testCase "RGB -> YCbCr Isomorphism" $ iso (255,255,255)
     ]
   ]
 
---iso :: Bool
-iso :: Assertion
-iso = (r,g,b) @?= (toR y' cr, toG y' cb cr, toB y' cb)
-  where r = M.fromVector (1,1) $ V.singleton 110
-        g = M.fromVector (1,1) $ V.singleton 115
-        b = M.fromVector (1,1) $ V.singleton 200
+iso :: (Word8,Word8,Word8) -> Assertion
+iso (x,y,z) = (toR y' cr, toG y' cb cr, toB y' cb) @?= (r,g,b)
+  where r = M.fromVector (1,1) $ V.singleton x
+        g = M.fromVector (1,1) $ V.singleton y
+        b = M.fromVector (1,1) $ V.singleton z
         y' = toY r g b
         cr = toCr r g b
         cb = toCb r g b
