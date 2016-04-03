6d49b45ba7b07aa87f7d902ef502c10e01fcb7e0
Colin Woodbury <colingw@gmail.com>
2016-04-03 13:44:48 -0700

    (412 - A3) Changed `histtrack` function signature

--- cmpt412/a3/histtrack.m
+++ cmpt412/a3/histtrack.m
@@ -2,15 +2,7 @@
 % Track an object through a given video.
 % author: Colin Woodbury <cwoodbur@sfu.ca>
 
-function [XS,YS] = histtrack(vidFile, xStart, xEnd, yStart, yEnd)
-
-V = importdata(vidFile);
-I = rgb2ycbcr(V(xStart:xEnd, yStart:yEnd, 1:3, 1));  % The model matrix.
-M = histogram(I);  % Model histogram
-
-figure
-imshow(ycbcr2rgb(I));
-
+function [XS,YS] = histtrack(V, M)
 XS = [];
 YS = [];
 
@@ -24,11 +16,3 @@ for i=1:size(V,4)
     XS = [XS; X];
     YS = [YS; Y];
 end
-
-figure
-imshow(ycbcr2rgb(Fimg));
-hold on;
-p = plot([YS], [XS], 'o');
-p.MarkerEdgeColor = 'red';
-p.MarkerSize = 10;
-hold off;
--- cmpt412/a3/trackdemo.m
+++ cmpt412/a3/trackdemo.m
@@ -2,4 +2,8 @@
 
 clear;
 
-[XS,YS] = histtrack('bluecup.mat', 178, 318, 31, 148);
+V = importdata('bluecup.mat');
+I = rgb2ycbcr(V(178:318, 31:148, 1:3, 1));  % The model matrix.
+M = histogram(I);  % Model histogram
+
+[XS,YS] = histtrack(V, M);
